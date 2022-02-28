2d2815b598b7e5a76edb46e9885bc342651551c7
OliveiraGusta <gustavo.oliver456@gmail.com>
2022-02-28 16:40:42 -0300

    Learn good habits

--- hello_flutter/lib/main.dart
+++ hello_flutter/lib/main.dart
@@ -1,10 +1,12 @@
+import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-main(){
-  runApp(Container(
-    child: Text (
-      'Gustavo',
-      textDirection: TextDirection.ltr,),
-  ));
+import 'app_widget.dart';
+
+main() {
+  runApp(AppWidget(title: '',));
 }
+
+
