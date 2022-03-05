e706a338ca9636d087b3c132eff93dfd2a7ab2d5
OliveiraGusta <gustavo.oliver456@gmail.com>
2022-03-05 00:44:53 -0300

    Method _body

--- hello_flutter/lib/login_page.dart
+++ hello_flutter/lib/login_page.dart
@@ -11,60 +11,68 @@ class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 200,
-                  height: 300,
-                  child: Image.asset('../assets/images/logo.png'),
-                ),
-                Container(height: 10),
-                TextField(
-                  onChanged: (text) {
-                    email = text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
+  Widget _body() {
+    return Column(
+      children: [
+        SingleChildScrollView(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 200,
+                    height: 300,
+                    child: Image.asset('../assets/images/logo.png'),
                   ),
-                ),
-                Container(height: 10),
-                TextField(
-                  onChanged: (text) {
-                    password = text;
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                Container(height: 15),
-                RaisedButton(
-                    onPressed: () {
-                      if (email == 'gustavo@etec.com' && password == '123g') {
-                        Navigator.of(context).pushReplacementNamed('/home');
-                      } else {
-                        print('login invalido');
-                      }
+                  Container(height: 10),
+                  TextField(
+                    onChanged: (text) {
+                      email = text;
                     },
-                    child: Text('Entrar'))
-              ],
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  Container(height: 10),
+                  TextField(
+                    onChanged: (text) {
+                      password = text;
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  Container(height: 15),
+                  RaisedButton(
+                      onPressed: () {
+                        if (email == 'gustavo@etec.com' && password == '123g') {
+                          Navigator.of(context).pushReplacementNamed('/home');
+                        } else {
+                          print('login invalido');
+                        }
+                      },
+                      child: Text('Entrar'))
+                ],
+              ),
             ),
           ),
         ),
-      ),
+      ],
     );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body:
+     _body());
   }
 }
