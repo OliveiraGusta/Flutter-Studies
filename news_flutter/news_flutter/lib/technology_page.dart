a9b45f6bbc9034ff8bafb415e9f3cca889b26117
OliveiraGusta <gustavo.oliver456@gmail.com>
2022-03-16 18:47:33 -0300

    Finished Technology

--- news_flutter/news_flutter/lib/technology_page.dart
+++ news_flutter/news_flutter/lib/technology_page.dart
@@ -90,8 +90,7 @@ class _TechnologyPageState extends State<TechnologyPage> {
       child: Column(
         children: [
           //1º NOTICIA
-          
-Padding(
+          Padding(
             padding: const EdgeInsets.all(8.0),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(30),
@@ -110,18 +109,18 @@ Padding(
                               ClipRRect(
                                 borderRadius: BorderRadius.circular(40),
                                 child: Image.network(
-                                    'https://cdn.ome.lt/xKT3NZ8b_pN4wsdqpdFj2ocDDts=/1200x630/smart/extras/conteudos/FIFA_22.png'),
+                                    "https://img.olhardigital.com.br/wp-content/uploads/2022/02/shutterstock_1975824899-1000x450.jpg"),
                               ),
                               SizedBox(height: 10),
                               Text(
-                                "FIFA quer 1 bilhão de dólares para manter marca da organização na série de jogos de futebol da Electronic Arts",
+                                "Número de cadastros com biometria quase triplica no sistema GovBR",
                                 textAlign: TextAlign.center,
                               ),
                             ],
                           ),
                         ),
                         content: Text(
-                          "O valor é o dobro do acordo atual. A EA não deve aceitar os novos termos e poderá mudar o nome do jogo para “EA Sports F.C.”. O primeiro jogo da série, “FIFA International Soccer”, foi lançado há quase 30 anos, em 1993. As informações são do The New York Times.",
+                          "O motivo é a exigência de assinaturas avançadas para acesso a serviços públicos pela internet. Atualmente já existem 15 milhões de cadastros com nível de segurança “Ouro”, 11,8 milhões no nível “Prata” e 99,2 milhões “Bronze”. A identidade digital de nível Ouro requer biometria facial, a Prata se refere a cadastros acessados com conta e senha de bancos integrados à plataforma e Bronze é o cadastro básico de login e senha. As informações são do site Convergência Digital.",
                           textAlign: TextAlign.center,
                         ),
                       );
@@ -144,7 +143,7 @@ Padding(
                         child: ClipRRect(
                           borderRadius: BorderRadius.circular(40),
                           child: Image.network(
-                              'https://cdn.ome.lt/xKT3NZ8b_pN4wsdqpdFj2ocDDts=/1200x630/smart/extras/conteudos/FIFA_22.png'),
+                              "https://img.olhardigital.com.br/wp-content/uploads/2022/02/shutterstock_1975824899-1000x450.jpg"),
                         ),
                       ),
                       SizedBox(height: 2),
@@ -153,13 +152,13 @@ Padding(
                         child: Column(
                           children: [
                             Text(
-                              "FIFA quer 1 bilhão de dólares para manter marca da organização na série de jogos de futebol da Electronic Arts",
+                              "Número de cadastros com biometria quase triplica no sistema GovBR",
                               textAlign: TextAlign.center,
                               style: TextStyle(fontWeight: FontWeight.bold),
                             ),
                             SizedBox(height: 10),
                             Text(
-                              "O valor é o dobro do acordo atual. A EA não deve aceitar os novos termos e poderá mudar o nome do jogo para “EA Sports F.C.”. O primeiro jogo da série, “FIFA International Soccer”, foi lançado há quase 30 anos, em 1993. As informações são do The New York Times.",
+                              "O motivo é a exigência de assinaturas avançadas para acesso a serviços públicos pela internet. Atualmente já existem 15 milhões de cadastros com nível de segurança “Ouro”, 11,8 milhões no nível “Prata” e 99,2 milhões “Bronze”. A identidade digital de nível Ouro requer biometria facial, a Prata se refere a cadastros acessados com conta e senha de bancos integrados à plataforma e Bronze é o cadastro básico de login e senha. As informações são do site Convergência Digital.",
                               textAlign: TextAlign.center,
                             ),
                           ],
@@ -171,7 +170,7 @@ Padding(
                             top: 20, right: 10, left: 10, bottom: 25),
                         child: ElevatedButton(
                           onPressed: () => _launcherLink(
-                              "https://www.google.com.br/search?q=FIFA+quer+1+bilh%C3%A3o+de+d%C3%B3lares+para+manter+marca+da+organiza%C3%A7%C3%A3o+na+s%C3%A9rie+de+jogos+de+futebol+da+Electronic+Arts&source=hp&ei=rgMyYtjCKZDK1sQPncCpyAE&iflsig=AHkkrS4AAAAAYjIRvrA9X2S0HM0qHVeV8REYm1KKNiFu&ved=0ahUKEwjY0cDF-sr2AhUQpZUCHR1gChkQ4dUDCAc&uact=5&oq=FIFA+quer+1+bilh%C3%A3o+de+d%C3%B3lares+para+manter+marca+da+organiza%C3%A7%C3%A3o+na+s%C3%A9rie+de+jogos+de+futebol+da+Electronic+Arts"),
+                              "https://www.google.com/search?client=opera-gx&q=Número+de+cadastros+com+biometria+quase+triplica+no+sistema+GovBR&sourceid=opera&ie=UTF-8&oe=UTF-8"),
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
@@ -195,8 +194,221 @@ Padding(
               ),
             ),
           ),
-
           //FIM 1 NOTICIA
+
+          //2º NOTICIA
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child:
+                  //Start Dialog
+                  GestureDetector(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(40),
+                                child: Image.network(
+                                    "https://pplware.sapo.pt/wp-content/uploads/2022/03/facebook_00-1.jpg"),
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                "Facebook e Instagram permitirão postagens incentivando violência contra russos",
+                                textAlign: TextAlign.center,
+                              ),
+                            ],
+                          ),
+                        ),
+                        content: Text(
+                          "A Reuters teria obtido e-mails internos da Meta delineando as mudanças em políticas de discurso de ódio no contexto da guerra na Ucrânia. As mudanças seriam temporárias e se aplicariam a posts originados da Letônia, Lituânia, Estônia, Polônia, Eslováquia, Hungria, Romênia, Rússia e Ucrânia. As informações são da Reuters.",
+                          textAlign: TextAlign.center,
+                        ),
+                      );
+                    },
+                  );
+                },
+                //End Dialog
+                //Card
+                child: Card(
+                  elevation: 10,
+                  margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 16.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(0.0),
+                  ),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 20, right: 10, bottom: 5, left: 10),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(40),
+                          child: Image.network(
+                              "https://pplware.sapo.pt/wp-content/uploads/2022/03/facebook_00-1.jpg"),
+                        ),
+                      ),
+                      SizedBox(height: 2),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              "Facebook e Instagram permitirão postagens incentivando violência contra russos",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              "A Reuters teria obtido e-mails internos da Meta delineando as mudanças em políticas de discurso de ódio no contexto da guerra na Ucrânia. As mudanças seriam temporárias e se aplicariam a posts originados da Letônia, Lituânia, Estônia, Polônia, Eslováquia, Hungria, Romênia, Rússia e Ucrânia. As informações são da Reuters.",
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.bottomCenter,
+                        margin: EdgeInsets.only(
+                            top: 20, right: 10, left: 10, bottom: 25),
+                        child: ElevatedButton(
+                          onPressed: () => _launcherLink(
+                              "https://www.google.com/search?q=Facebook+e+Instagram+permitirão+postagens+incentivando+violência+contra+russos&source=lmns&bih=627&biw=1325&client=opera-gx&hs=jDK&hl=pt-BR&sa=X&ved=2ahUKEwiHms6Lx8v2AhXpiZUCHabHBZAQ_AUoAHoECAEQAA"),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.search_outlined),
+                              Padding(
+                                padding: EdgeInsets.all(16),
+                                child: Text(
+                                  'Pesquisar Sobre',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w100),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          //FIM 2 NOTICIA
+
+          //3º NOTICIA
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child:
+                  //Start Dialog
+                  GestureDetector(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(40),
+                                child: Image.network(
+                                    "https://img.olhardigital.com.br/uploads/acervo_imagens/2015/10/20151008173723.jpg"),
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                "Garoto de 13 anos inventa “bafômetro” que utiliza inteligência artificial para detectar marcadores de pneumonia fúngica",
+                                textAlign: TextAlign.center,
+                              ),
+                            ],
+                          ),
+                        ),
+                        content: Text(
+                          "Caleb combinou um projeto open source de nariz artificial com uma incubadora que detecta mudanças nas concentrações de compostos orgânicos voláteis na respiração e os correlaciona a processos metabólicos e patofisiológicos no corpo. Seu modelo pode ainda ser mais barato e mais facilmente empregado do que métodos utilizados hoje. Caleb decidiu criar o dispositivo após ter pneumonia aos nove anos de idade e aprofundar sua pesquisa em um projeto de ciências na escola onde estuda, nos EUA. As informações são do blog de Benjamin Cabé.",
+                          textAlign: TextAlign.center,
+                        ),
+                      );
+                    },
+                  );
+                },
+                //End Dialog
+                //Card
+                child: Card(
+                  elevation: 10,
+                  margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 16.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(0.0),
+                  ),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 20, right: 10, bottom: 5, left: 10),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(40),
+                          child: Image.network(
+                              "https://img.olhardigital.com.br/uploads/acervo_imagens/2015/10/20151008173723.jpg"),
+                        ),
+                      ),
+                      SizedBox(height: 2),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              "Garoto de 13 anos inventa “bafômetro” que utiliza inteligência artificial para detectar marcadores de pneumonia fúngica",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              "Caleb combinou um projeto open source de nariz artificial com uma incubadora que detecta mudanças nas concentrações de compostos orgânicos voláteis na respiração e os correlaciona a processos metabólicos e patofisiológicos no corpo. Seu modelo pode ainda ser mais barato e mais facilmente empregado do que métodos utilizados hoje. Caleb decidiu criar o dispositivo após ter pneumonia aos nove anos de idade e aprofundar sua pesquisa em um projeto de ciências na escola onde estuda, nos EUA. As informações são do blog de Benjamin Cabé.",
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.bottomCenter,
+                        margin: EdgeInsets.only(
+                            top: 20, right: 10, left: 10, bottom: 25),
+                        child: ElevatedButton(
+                          onPressed: () => _launcherLink(
+                              "https://www.google.com/search?q=Garoto+de+13+anos+inventa+“bafômetro”+que+utiliza+inteligência+artificial+para+detectar+marcadores+de+pneumonia+fúngica&source=lmns&bih=627&biw=1325&client=opera-gx&hs=aFK&hl=pt-BR&sa=X&ved=2ahUKEwjb4Ne-x8v2AhUskZUCHQvsB-sQ_AUoAHoECAEQAA"),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.search_outlined),
+                              Padding(
+                                padding: EdgeInsets.all(16),
+                                child: Text(
+                                  'Pesquisar Sobre',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w100),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          //FIM 3° NOTICIA
         ],
       ),
     );
